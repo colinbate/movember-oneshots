7b5c398dca281644a9ea4886c455160680591d43
Colin Bate <colin@colinbate.com>
2022-10-06 17:59:53 -0300

    Initial commit

---
+++ src/components/Date.svelte
@@ -0,0 +1,9 @@
+<script>
+export let date;
+const parsed = new Date(date);
+const localized = parsed.toLocaleString("en", {
+  timeStyle: "long",
+  dateStyle: "full"
+});
+</script>
+<time date={date} title={date}>{localized}</time>

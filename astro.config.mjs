375dc94cf19af4ec1374fbaba5501c4d2e87a87b
Colin Bate <colin@colinbate.com>
2023-10-19 16:17:24 -0300

    Add scene image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), svelte()]
+  integrations: [mdx(), svelte()],
+  image: {
+    domains: ['longrest.nyc3.digitaloceanspaces.com']
+  }
 });
